9f27488ff09b2e1895405a13000141bc64ed7081
ckhui <ckh@gmail.com>
2019-07-09 09:46:47 +0800

    add 404 and 404shortcut

--- django/django_app/polls/views.py
+++ django/django_app/polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from django.template import loader
 from .models import Question
 
@@ -13,8 +13,22 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
+def index_renderShortcut(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+def detail_404Shortcut(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
